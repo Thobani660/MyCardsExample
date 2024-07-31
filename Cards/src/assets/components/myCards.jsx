b52215d616fea3cd67ba3694631516e51155842b
Thobani660 <zondit660@gmail.com>
2024-07-31 09:47:20 +0200

    latest

---
+++ Cards/src/assets/components/myCards.jsx
@@ -0,0 +1,13 @@
+function Card(){
+    return(
+        <>
+        <div className="Card">
+            <h1>hello</h1>
+            <div className="Cardpic"></div>
+            <div className="Cardprice"></div>
+            <div className="Cardrooms"></div>
+            <div className="Cardcontacts"></div>
+        </div>
+        </>
+    )
+}export default Card;
